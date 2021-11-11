7bcd171832a93cb019499542ba010c96b93962ad
neoruien <neoruien123@gmail.com>
2021-11-11 21:25:43 +0800

    Add animate css

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,20 @@
 import logo from "../img/Logo Heart.png";
 import illustration from "../img/Sewing Illustration.svg";
 import NextButton from "../components/NextButton";
+import { Animated } from "react-animated-css";
 
 function HomePage() {
   return (
     <div className="HomePage">
-      <div className="home-content">
-        <img src={logo} alt="logo" className="home-logo" />
-        <p className="home-text">Welcome to The Seams</p>
-        <p className="home-subtext">we keep things seample</p>
-        <NextButton to="/problem" title="Begin Journey" />
-      </div>
+      <Animated animationIn="fadeInLeft" isVisible={true}>
+        <div className="home-content">
+          <img src={logo} alt="logo" className="home-logo" />
+          <p className="home-text">Welcome to The Seams</p>
+          <p className="home-subtext">we keep things seample</p>
+          <NextButton to="/problem" title="Begin Journey" />
+        </div>
+      </Animated>
+
       <img src={illustration} alt="illustration" className="illustration" />
     </div>
   );
